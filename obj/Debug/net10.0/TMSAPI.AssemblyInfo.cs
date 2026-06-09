36129060599cf9ecddc0f0a5289e066b450b7bb7
beka_tech <abeka039@gmail.com>
2026-06-09 10:14:21 +0300

    Custom TrainingAuthHandler

--- obj/Debug/net10.0/TMSAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/TMSAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TMSAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+868d9d474cbe6b9dbdd5c00807d74ef530a4c8e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b1434361d6ecf398da0192dee56f0d669853b61")]
 [assembly: System.Reflection.AssemblyProductAttribute("TMSAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TMSAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
