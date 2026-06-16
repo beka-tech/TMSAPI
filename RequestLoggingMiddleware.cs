a10cc91f495640197e54f68df4c5544a4621c6f1
beka_tech <abeka039@gmail.com>
2026-06-16 06:55:03 +0300

    Dependency Injection and Service LifeTimes

--- RequestLoggingMiddleware.cs
+++ RequestLoggingMiddleware.cs
@@ -1,4 +1,3 @@
-
 using System.Diagnostics;
 
 public class RequestLoggingMiddleware
@@ -14,23 +13,26 @@ public class RequestLoggingMiddleware
 
     public async Task InvokeAsync(HttpContext context)
     {
-        // 1. Generate Correlation ID and set the response header immediately
         string correlationId = Guid.NewGuid().ToString("N")[..8];
         context.Response.Headers["X-Correlation-Id"] = correlationId;
 
-        // 2. Start measuring time
         var sw = Stopwatch.StartNew();
 
-        // 3. Log the entry
-        _logger.LogInformation("Start Request: {Method} {Path} [CorrelationId: {CorrelationId}]", 
-            context.Request.Method, context.Request.Path, correlationId);
+        _logger.LogInformation(
+            "Start Request: {Method} {Path} [CorrelationId: {CorrelationId}]",
+            context.Request.Method,
+            context.Request.Path,
+            correlationId
+        );
 
-        // 4. Pass control to the next middleware in the pipeline
         await _next(context);
 
-        // 5. Stop measuring and log the exit
         sw.Stop();
-        _logger.LogInformation("End Request: {StatusCode} in {ElapsedMs}ms [CorrelationId: {CorrelationId}]", 
-            context.Response.StatusCode, sw.ElapsedMilliseconds, correlationId);
+        _logger.LogInformation(
+            "End Request: {StatusCode} in {ElapsedMs}ms [CorrelationId: {CorrelationId}]",
+            context.Response.StatusCode,
+            sw.ElapsedMilliseconds,
+            correlationId
+        );
     }
 }
